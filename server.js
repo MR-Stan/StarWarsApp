c2b7cdda38a25d7c8768f79ae0cecf9c718cc4cf
MR-Stan <michaelrstanley87@gmail.com>
2019-11-23 13:40:45 -0600

    updated PORT

--- server.js
+++ server.js
@@ -42,21 +42,21 @@ var characters = [
 // =============================================================
 
 // Basic route that sends the user first to the AJAX Page
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "view.html"));
 });
 
-app.get("/add", function(req, res) {
+app.get("/add", function (req, res) {
   res.sendFile(path.join(__dirname, "add.html"));
 });
 
 // Displays all characters
-app.get("/api/characters", function(req, res) {
+app.get("/api/characters", function (req, res) {
   return res.json(characters);
 });
 
 // Displays a single character, or returns false
-app.get("/api/characters/:character", function(req, res) {
+app.get("/api/characters/:character", function (req, res) {
   var chosen = req.params.character;
 
   console.log(chosen);
@@ -71,7 +71,7 @@ app.get("/api/characters/:character", function(req, res) {
 });
 
 // Create New Characters - takes in JSON input
-app.post("/api/characters", function(req, res) {
+app.post("/api/characters", function (req, res) {
   // req.body hosts is equal to the JSON post sent from the user
   // This works because of our body parsing middleware
   var newCharacter = req.body;
@@ -89,6 +89,6 @@ app.post("/api/characters", function(req, res) {
 
 // Starts the server to begin listening
 // =============================================================
-app.listen(PORT, function() {
+app.listen(process.env.PORT || 3000, function () {
   console.log("App listening on PORT " + PORT);
 });
